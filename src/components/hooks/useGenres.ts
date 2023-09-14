1f31271bbf5aa5f5e613a3c0ed784de9f0597608
Kaushik Karuturi <kaushikchowdary99@gmail.com>
2023-09-13 20:05:41 -0700

    Displaying Genres

--- src/components/hooks/useGenres.ts
+++ src/components/hooks/useGenres.ts
@@ -5,29 +5,40 @@ import { CanceledError } from "axios";
 interface Genre {
     id: number
     name: string;
+    image_background: string;
 }
 
 interface FetchGenresResponse {
     count: number;
-    results: Genre[]
+    results: Genre[];
+    image_background: string;
 }
 
 const useGenres = () => {
     const [genres, setGenres] = useState<Genre[]>([])
     const [error, setError] = useState("");
+    const [isLoading, setLoading] = useState(false);
+
     useEffect(() => {
         const controller = new AbortController();
 
+        setLoading(true);
         apiClient.get<FetchGenresResponse>("/genres", { signal: controller.signal })
-        .then(res => setGenres(res.data.results))
-        .catch(err => {
-            if (err instanceof CanceledError) return;
-            setError(err);
-            console.log("Error:", err.message);
+        .then(res => {
+            setGenres(res.data.results); 
+            setLoading(false)
         })
-    })
+        .catch((err) => {
+            if (err instanceof CanceledError) return;
+            setError(err.message);
+            console.error("Error:", err.message);
+            setLoading(false);
+        });
 
-    return {genres, error}
+        return () => controller.abort();
+        },[])
+
+    return {genres, error, isLoading}
 }
 
 export default useGenres;
