bb4ca6fcd58dd05510cad68aca3f87a0354cab89
Kaushik Karuturi <kaushikchowdary99@gmail.com>
2023-09-27 23:37:02 -0700

    Implemented Infinite queries

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,14 @@ import theme from "./theme.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 10 * 1000 * 60 * 60 * 24, //24hrs
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
