4e0387c82db347938b226bbb0471295d9b7dbff3
Kaushik Karuturi <kaushikchowdary99@gmail.com>
2023-10-11 00:36:03 -0700

    added game trailers

--- src/components/GameAttributes.tsx
+++ src/components/GameAttributes.tsx
@@ -12,17 +12,20 @@ const GameAttributes = ({ game }: Props) => {
     <SimpleGrid columns={2} as="dl">
       <DefinitionItem term="Platforms">
         {game.parent_platforms?.map(({ platform }) => (
-          <Text key="platform.id">{platform.name}</Text>
+          <Text key={platform.id}>{platform.name}</Text>
         ))}
       </DefinitionItem>
+
       <DefinitionItem term="Metascore">
         <CriticScore score={game.metacritic} />
       </DefinitionItem>
+
       <DefinitionItem term="Genres">
         {game.genres.map((genre) => (
           <Text key={genre.id}>{genre.name}</Text>
         ))}
       </DefinitionItem>
+
       <DefinitionItem term="Publishers">
         {game.publishers?.map((publisher) => (
           <Text key={publisher.id}>{publisher.name}</Text>
